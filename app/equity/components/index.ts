2522607063e976e5b56d888c7568d20a99f59d60
DakshGupta1204 <dakshgpt12@gmail.com>
2025-08-09 02:14:57 +0530

    Fix equity components index.ts
    - Restored missing index.ts file for equity components
    - Updated exports to match available components
    - Removed references to deleted RecommendationCard component
    - Added exports for EquityHeroSection and BackgroundVideo components

--- app/equity/components/index.ts
+++ app/equity/components/index.ts
@@ -1,6 +1,7 @@
 export { default as PropertyCard } from "./PropertyCard";
 export { default as PortfolioSummary } from "./PortfolioSummary";
 export { default as PerformanceChart } from "./PerformanceChart";
-export { default as RecommendationCard } from "./RecommendationCard";
 export { default as EquityNavigation } from "./EquityNavigation";
+export { default as EquityHeroSection } from "./EquityHeroSection";
 export { default as AuthModal } from "./AuthModal";
+export { default as BackgroundVideo } from "./BackgroundVideo";
